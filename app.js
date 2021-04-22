1530c1069dc35e8698bdc9ab807fc8fc9663accd
shivam-techie <82820941+shivam-techie@users.noreply.github.com>
2021-04-22 15:58:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/trains', trainsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(400));
+  next(createError(402));
 });
 
 // error handler
